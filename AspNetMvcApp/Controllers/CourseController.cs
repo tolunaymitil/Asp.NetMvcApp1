30ecc2bac8de754bbf4e3aa3861804a6adcbeebd
ACER <ACER@ACER-BILGISAYAR>
2022-02-21 14:58:41 +0200

    Add project files.

---
+++ AspNetMvcApp/Controllers/CourseController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetMvcApp.Controllers
+{
+    public class CourseController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour > 12 ? "Good Afternoon " : "Good Morning";
+            return View("MyView");
+        }
+        public ViewResult List()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ AspNetMvcApp/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using AspNetMvcApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetMvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var course = new Course();
+            course.Id = 1;
+            course.Name = "Asp.Net COre";
+
+            return View();
+        }
+    }
+}
